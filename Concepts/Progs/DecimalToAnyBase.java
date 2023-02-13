7e69b1fc95d2d3a6816620bc249e7adc42e48ae5
Sashi Adhithya <70097892+adhithyasash1@users.noreply.github.com>
2023-02-13 13:11:22 +0530

    Update DecimalToAnyBase.java

--- Concepts/Progs/DecimalToAnyBase.java
+++ Concepts/Progs/DecimalToAnyBase.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-/** @author Varun Upadhyay (https://github.com/varunu28) */
+
 
 // Driver Program
 public class DecimalToAnyBase {
